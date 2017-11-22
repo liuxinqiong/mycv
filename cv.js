f975d07e3c205302866bcdecdf76144fd24308c6
liuxinqiong <372513646@qq.com>
2017-11-22 23:25:19 +0800

    my resume v1.0

---
+++ cv.js
@@ -0,0 +1,18 @@
+/**
+ * Created by sky on 2017/11/22.
+ */
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8888, function () {
+    console.log('mycv listening on port 8888');
+});
